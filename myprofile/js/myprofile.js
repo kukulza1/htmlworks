68ed60f5425d10131976a51227280c8d8814e592
kukulza1 <wlskrkek12345@naver.com>
2023-11-14 17:47:36 +0900

    ss

---
+++ myprofile/js/myprofile.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+  let picture = ["img/bg0.jpg","img/bg1.jpg","img/bg5.jpg"
+                  ,"img/prod1.jpg"];
+
+  let i= 0;
+ showSlide();
+
+  function showSlide(){
+    document.getElementById("pic").src=picture[i];    
+    i++
+    if(i == picture.length){ i=0;}
+  
+    setTimeout(showSlide,2000);
+  }
+setInterval(myWatch,1000);
+
+function myWatch(){
+  const now = new Date();//날짜와 시간이 함께생성
+  let time = now.toLocaleTimeString(); //시간만 출력
+  console.log(time);
+
+  let watch = document.getElementById("show")
+  watch.innerHTML = time;
+  watch.style.color="orangered";
+}
+}
